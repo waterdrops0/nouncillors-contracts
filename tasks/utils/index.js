75eb82e33b70c8269bf01c6370a89f00d3350c37
curelycue <eduard02stoica@gmail.com>
2024-01-09 09:50:38 +0200

    Updated tasks utils/

--- tasks/utils/index.js
+++ tasks/utils/index.js
@@ -1,58 +1,5 @@
 const ethers = require('ethers');
-const promptjs = require('prompt');
 const { deflateRawSync } = require('zlib');
-
-
-promptjs.colors = false;
-promptjs.message = '> ';
-promptjs.delimiter = '';
-
-async function getGasPriceWithPrompt(ethers) {
-  const gasPrice = await ethers.provider.getGasPrice();
-  const gasInGwei = Math.round(Number(ethers.utils.formatUnits(gasPrice, 'gwei')));
-
-  promptjs.start();
-
-  let result = await promptjs.get([
-    {
-      properties: {
-        gasPrice: {
-          type: 'integer',
-          required: true,
-          description: 'Enter a gas price (gwei)',
-          default: gasInGwei,
-        },
-      },
-    },
-  ]);
-
-  return ethers.utils.parseUnits(result.gasPrice.toString(), 'gwei');
-}
-
-async function getDeploymentConfirmationWithPrompt() {
-  const result = await promptjs.get([
-    {
-      properties: {
-        confirm: {
-          type: 'string',
-          description: 'Type "DEPLOY" to confirm:',
-        },
-      },
-    },
-  ]);
-
-  return result.confirm == 'DEPLOY';
-}
-
-async function printEstimatedCost(factory, gasPrice) {
-  const deploymentGas = await factory.signer.estimateGas(
-    factory.getDeployTransaction({ gasPrice }),
-  );
-  const deploymentCost = deploymentGas.mul(gasPrice);
-  console.log(
-    `Estimated cost to deploy NounsDAOLogicV2: ${utils.formatUnits(deploymentCost, 'ether')} ETH`,
-  );
-}
 
 function dataToDescriptorInput(data) {
   const abiCoder = ethers.AbiCoder.defaultAbiCoder()
@@ -72,27 +19,7 @@ function dataToDescriptorInput(data) {
   };
 }
 
-function printContractsTable(contracts) {
-  console.table(
-    Object.values(contracts).reduce(
-      (acc, contract) => {
-        acc[contract.name] = {
-          Address: contract.address,
-        };
-        if (contract.instance?.deployTransaction) {
-          acc[contract.name]['Deployment Hash'] = contract.instance.deployTransaction.hash;
-        }
-        return acc;
-      },
-      {},
-    ),
-  );
-}
 
 module.exports = {
-  getGasPriceWithPrompt,
-  getDeploymentConfirmationWithPrompt,
-  printEstimatedCost,
-  dataToDescriptorInput,
-  printContractsTable,
+  dataToDescriptorInput
 };
